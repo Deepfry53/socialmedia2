b3244aeb94847b32707b09413d80f4a9736ecf4c
Deep_fry <sdeepak8087@gmail.com>
2024-08-16 16:02:29 +0530

    first commit

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import AuthProvider from './context/authcontext'
+
 import { QueryProvider } from './lib/React-query/QueryProvider'
 
 
